f337c7b3bf8bf02860f18658796186bea9a3a284
ketamean <truongthanhtoan2003@gmail.com>
2023-07-14 20:41:19 +0700

    commit

--- Code/implementation.cpp
+++ Code/implementation.cpp
@@ -3,20 +3,15 @@
 using namespace ns_priority_queue;
 using namespace global_variables;
 
-short ns_priority_queue::compare_priority(Patient *p1, Patient *p2)
-{
+short ns_priority_queue::compare_priority(Patient* p1, Patient* p2) {
     // p1 and p2 are both either vip or non-vip
 
-    if ((p1->prior_ord_emergency >= 0) ^ (p2->prior_ord_emergency >= 0))
-    {
+    if ((p1->prior_ord_emergency >= 0) ^ (p2->prior_ord_emergency >= 0)) {
         // if one of them is emergency and the other is not
-        if (p1->prior_ord_emergency > p2->prior_ord_emergency)
-        {
+        if (p1->prior_ord_emergency > p2->prior_ord_emergency) {
             // p1 is emergency and p2 is not
             return 1;
-        }
-        else
-        {
+        } else {
             // p2 is emergency and p1 is not
             return -1;
         }
@@ -40,20 +35,16 @@ short ns_priority_queue::compare_priority(Patient *p1, Patient *p2)
         return 1;
     else if (p1->prior_ord_normal < p2->prior_ord_normal)
         return -1;
-
+    
     // this case will never occur
     return 0;
 }
 
-Node *ns_priority_queue::merge1(Node *h1, Node *h2)
-{
-    if (h1->left == nullptr)
-        h1->left = h2;
-    else
-    {
+Node* ns_priority_queue::merge1(Node *h1, Node *h2) {
+    if (h1->left == nullptr) h1->left = h2;
+    else {
         h1->right = ns_priority_queue::merge(h1->right, h2);
-        if (h1->left->npl < h1->right->npl)
-        {
+        if (h1->left->npl < h1->right->npl) {
             swap(h1->left, h1->right);
             h1->npl = h1->right->npl + 1;
         }
@@ -61,8 +52,7 @@ Node *ns_priority_queue::merge1(Node *h1, Node *h2)
     return h1;
 }
 
-Node *ns_priority_queue::merge(Node *h1, Node *h2)
-{
+Node* ns_priority_queue::merge(Node *h1, Node *h2) {
     if (h1 == nullptr)
         return h2;
     else if (h2 == nullptr)
@@ -73,9 +63,8 @@ Node *ns_priority_queue::merge(Node *h1, Node *h2)
         return ns_priority_queue::merge1(h2, h1);
 }
 
-void ns_priority_queue::insertion(PriorityQueue *&pq, Patient *item)
-{
-    Node *node = new Node;
+void ns_priority_queue::insertion(PriorityQueue* &pq, Patient* item) {
+    Node* node = new Node;
     node->patient = item;
     node->left = node->right = nullptr;
     node->npl = 0;
@@ -83,11 +72,9 @@ void ns_priority_queue::insertion(PriorityQueue *&pq, Patient *item)
     pq->total_patients++;
 }
 
-void ns_priority_queue::deletion(PriorityQueue *&pq)
-{
-    if (pq->top)
-    {
-        Node *tmp = pq->top;
+void ns_priority_queue::deletion(PriorityQueue* &pq) { 
+    if (pq->top) {
+        Node* tmp = pq->top;
         pq->top = ns_priority_queue::merge(pq->top->left, pq->top->right);
         delete tmp->patient;
         delete tmp;
@@ -95,25 +82,19 @@ void ns_priority_queue::deletion(PriorityQueue *&pq)
     }
 }
 
-Patient *ns_priority_queue::peak(PriorityQueue *pq)
-{
-    if (pq->top)
-        return pq->top->patient;
+Patient* ns_priority_queue::peak(PriorityQueue* pq) {
+    if (pq->top) return pq->top->patient;
     return nullptr;
 }
 
-bool ns_priority_queue::is_empty(PriorityQueue *pq)
-{
-    if (pq->top == nullptr)
-        return true;
+bool ns_priority_queue::is_empty(PriorityQueue* pq) {
+    if (pq->top == nullptr) return true;
     return false;
 }
 
-void ns_priority_queue::delete_all_priority_queue(PriorityQueue *&pq)
-{
-    while (!ns_priority_queue::is_empty(pq))
-        ns_priority_queue::deletion(pq);
-    delete pq;
+void ns_priority_queue::delete_all_priority_queue(PriorityQueue* &pq) {
+    while (!ns_priority_queue::is_empty(pq)) ns_priority_queue::deletion(pq);
+        delete pq;
     pq = nullptr;
 }
 
@@ -135,36 +116,31 @@ void ns_priority_queue::delete_all_priority_queue(PriorityQueue *&pq)
 
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
-void generate_command_arguments(char cmd[], int &argc, char *argv[])
-{
+void generate_command_arguments(char cmd[], int &argc, char* argv[]) {
     argc = 0;
-    char *pch = strtok(cmd, " ");
-    while (pch != nullptr)
-    {
+    char* pch = strtok(cmd, " ");
+    while (pch != nullptr) {
         argv[argc++] = pch;
         pch = strtok(nullptr, " ");
     }
 }
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
-void open_medical_room(int number_of_regular_room, int number_of_vip_room)
-{
+void open_medical_room(int number_of_regular_room, int number_of_vip_room) {
     regular_room.reserve(number_of_regular_room);
-    Room *r = nullptr;
-    for (int i = 0; i < number_of_regular_room; ++i)
-    {
+    Room* r = nullptr;
+    for (int i = 0; i < number_of_regular_room; ++i) {
         r = new Room;
         r->type = 'R';
-        r->no = i + 1;
+        r->no = i+1;
         regular_room.push_back(r);
     }
 
     vip_room.reserve(number_of_vip_room);
-    for (int i = 0; i < number_of_regular_room; ++i)
-    {
+    for (int i = 0; i < number_of_regular_room; ++i) {
         r = new Room;
         r->type = 'R';
-        r->no = i + 1;
+        r->no = i+1;
         vip_room.push_back(r);
     }
 }
@@ -189,10 +165,8 @@ void print_one_patient(Patient* p) {
 }
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
-void preorder_print(Node *p)
-{
-    if (p == nullptr)
-    {
+void preorder_print(Node* p) {
+    if (p == nullptr) {
         cout << "End." << endl;
         return;
     }
@@ -203,80 +177,36 @@ void preorder_print(Node *p)
 }
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
-void print_patient_list(Room *room)
-{
+void print_patient_list(Room* room) {
     cout << "Room " << (char)room->type << room->no << ":" << endl;
     if (room->patients)
         preorder_print(room->patients->top);
 }
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
-Room *get_room(string room)
-{
+Room* get_room(string room) {
     char type_of_room = room[0];
     room.erase(room.begin());
-    if (type_of_room == 'R')
-    {
-        return regular_room[stoi(room) - 1];
-    }
-    else
-    {
-        return vip_room[stoi(room) - 1];
+    if (type_of_room == 'R') {
+        return regular_room[stoi(room)-1];
+    } else {
+        return vip_room[stoi(room)-1];
     }
 }
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
-bool do_the_task(int argc, char *argv[], bool print_new_patient)
-{
+bool do_the_task(int argc, char* argv[], bool print_new_patient) {
     string tmp_task = string(argv[0]);
     TASK task = get_task(tmp_task);
-    switch (task)
-    {
-    case Open:
-    {
-        if (argc == 3)
-        {
-            // > Open x y
-            open_medical_room(stoi(string(argv[1])), stoi(string(argv[2])));
-        }
-        else if (argc == 2)
-        {
-            // > Open x
-            open_medical_room(stoi(string(argv[1])), 0);
-        }
-    }
-    break;
-
-    case New:
-    {
-        Patient *p = nullptr;
-        if (argc == 2)
-        {
-            // > New filename
-            ifstream ifs;
-            ifs.open(string(argv[1]));
-            string tmp_str;
-            char *file_argv[6], *tmp_cstr;
-            int file_argc;
-
-            if (!ifs.eof())
+    switch (task) {
+        case Open:
             {
-                cout << "Cannot open file " << string(argv[1]) << endl;
-                break;
-            }
-            cout << "Adding patients..." << endl;
-            while (!ifs.eof())
-            {
-                getline(ifs, tmp_str);
-                strcpy(tmp_cstr, tmp_str.c_str());
-                generate_command_arguments(tmp_cstr, file_argc, file_argv);
-                do_the_task(file_argc, file_argv, true);
-
-                // deallocate argv[i]
-                while (file_argc > 0)
-                {
-                    file_argc--;
-                    delete[] file_argv[file_argc];
+                if (argc == 3) {
+                    // > Open x y
+                    open_medical_room(stoi(string(argv[1])), stoi(string(argv[2])));
+                } else if (argc == 2) {
+                    // > Open x
+                    open_medical_room(stoi(string(argv[1])), 0);
                 }
             }
             break;
@@ -414,113 +344,82 @@ bool do_the_task(int argc, char *argv[], bool print_new_patient)
                         print_one_patient(p);
                     }
                 }
-                else if (p->prior_ord_old >= 0)
-                {
-                    cout << "Old ";
-                }
-                else if (p->prior_ord_children >= 0)
-                {
-                    cout << "Children ";
-                }
-                else
-                {
-                    cout << "Normal patient ";
-                }
-
-                cout << p->room->type << p->room->no << endl;
             }
-        }
-    }
-    break;
+            break;
 
-    case See:
-    {
-        if (argv[1][0] == 'A')
-        {
-            // > See All
-            cout << "Regular medical room." << endl;
-            for (int i = 0; i < regular_room.size(); ++i)
+        case See:
             {
-                print_patient_list(regular_room[i]);
-                cout << "Number of waiting patients: ";
-                if (regular_room[i]->patients)
-                    cout << regular_room[i]->patients->total_patients << endl;
-                else
-                    cout << 0 << endl;
-            }
+                if (argv[1][0] == 'A') {
+                    // > See All
+                    cout << "Regular medical room." << endl;
+                    for (int i = 0; i < regular_room.size(); ++i) {
+                        print_patient_list(regular_room[i]);
+                        cout << "Number of waiting patients: ";
+                        if (regular_room[i]->patients)
+                            cout << regular_room[i]->patients->total_patients << endl;
+                        else
+                            cout << 0 << endl;
+                    }
 
-            if (vip_room.size() > 0)
+                    if (vip_room.size() > 0) {
+                        cout << endl << "VIP medical room." << endl;
+                    }
+                    for (int i = 0; i < vip_room.size(); ++i) {
+                        print_patient_list(vip_room[i]);
+                        cout << "Number of waiting patients: ";
+                        if (vip_room[i]->patients)
+                            cout << vip_room[i]->patients->total_patients << endl;
+                        else
+                            cout << 0 << endl;                    
+                    }
+                } else {
+                    // > See medical_room_ID
+                    Room* room = get_room(string(argv[1]));
+                    print_patient_list(room);
+                    cout << "Number of waiting patients: " << room->patients->total_patients << endl;
+                }
+            }
+            break;
+        
+        case Update:
             {
-                cout << endl
-                     << "VIP medical room." << endl;
+                if (argc == 3) {
+                    // > Update medical_room_id number_of_finished_treatments
+                    int i = 0;
+                    Room* room = get_room(string(argv[1]));
+                    update_medical_room(room, stoi(string(argv[2])));
+                }
             }
-            for (int i = 0; i < vip_room.size(); ++i)
+            break;
+
+        case Exit:
             {
-                print_patient_list(vip_room[i]);
-                cout << "Number of waiting patients: ";
-                if (vip_room[i]->patients)
-                    cout << vip_room[i]->patients->total_patients << endl;
-                else
-                    cout << 0 << endl;
+                return 0;
             }
-        }
-        else
-        {
-            // > See medical_room_ID
-            Room *room = get_room(string(argv[1]));
-            print_patient_list(room);
-            cout << "Number of waiting patients: " << room->patients->total_patients << endl;
-        }
-    }
-    break;
-
-    case Update:
-    {
-        if (argc == 3)
-        {
-            // > Update medical_room_id number_of_finished_treatments
-            int i = 0;
-            Room *room = get_room(string(argv[1]));
-            update_medical_room(room, stoi(string(argv[2])));
-        }
-    }
-    break;
-
-    case Exit:
-    {
-        return 0;
-    }
-    break;
-    default:
-    {
-        // the input command line is not valid
-        cout << "You entered a wrong command line." << endl;
-    }
+            break;
+        default:
+            {
+                // the input command line is not valid
+                cout << "You entered a wrong command line." << endl;
+            }
     } // end switch
     return 1;
 } // end function do_the_task()
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
-TASK get_task(string task)
-{
-    if (task == "New")
-        return New;
-    if (task == "See")
-        return See;
-    if (task == "Update")
-        return Update;
-    if (task == "Open")
-        return Open;
-    if (task == "Exit")
-        return Exit;
+TASK get_task(string task) {
+    if (task == "New") return New;
+    if (task == "See") return See;
+    if (task == "Update") return Update;
+    if (task == "Open") return Open;
+    if (task == "Exit") return Exit;
     return WRONG_CMD;
 } // end function get_task()
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
-void coordinate_patient_to_room(Patient *p)
-{
+void coordinate_patient_to_room(Patient* p) {
     // allocate a new Node
-    Node *new_item = new Node;
+    Node* new_item = new Node;
     new_item->patient = p;
     new_item->left = new_item->right = nullptr;
 
@@ -529,22 +428,14 @@ void coordinate_patient_to_room(Patient *p)
     p->room = room_to_add;
     if (p->prior_ord_emergency == 1) {
         p->prior_ord_emergency = room_to_add->ord_emergency++;
-    }
-    else if (p->prior_ord_old == 1)
-    {
+    } else if (p->prior_ord_old == 1) {
         p->prior_ord_old = room_to_add->ord_old++;
-    }
-    else if (p->prior_ord_children == 1)
-    {
+    } else if (p->prior_ord_children == 1) {
         p->prior_ord_children = room_to_add->ord_children++;
-    }
-    else
-    {
+    } else {
         p->prior_ord_normal = room_to_add->ord_normal++;
     }
-    cout << "insert" << endl;
-    ns_priority_queue::insertion(room_to_add->patients, p);
-    cout << "done insert" << endl;
+    insertion(room_to_add->patients, p);
 }
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
